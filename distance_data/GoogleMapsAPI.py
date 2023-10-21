7864bb42a8eae918e4d9200d20892d86d28ce658
Siddharth <siddharth.iyer123@gmail.com>
2023-10-21 15:56:52 -0500

    Apts to Schools all distances

--- distance_data/GoogleMapsAPI.py
+++ distance_data/GoogleMapsAPI.py
@@ -9,32 +9,39 @@ colleges = csv.DictReader(open('/Users/siddharthiyer/Documents/GitHub/wampusfyi/
 api_key = "AIzaSyDoPCPo-aK28JSPXhMRHBdzL8jCpjrpvfc"
 client = Client(api_key)
 
-df = pd.read_csv("C:/Users/sahil/Desktop/CS Files/wampusfyi/distance_data/apartment_addresses.csv")
+apt_df = pd.read_csv("distance_data/apartment_addresses.csv")
+college_df = pd.read_csv("distance_data/college_addresses.csv")
+
+# Upload coordinates to a table in the csv file
+geocode_result = []
+for i in apt_df["Address"]:
+    geocode_result.append(client.geocode(i))
+apt_df['coords'] = geocode_result
 
 geocode_result = []
+for i in college_df["Address"]:
+    geocode_result.append(client.geocode(i))
+college_df['coords'] = geocode_result
 
-for i in df["Address"]:
-        geocode_result.append(client.geocode(i))
+distance_df = pd.DataFrame(columns=['Apartment', 'School', 'Distance'])
 
-df['coords'] = geocode_result
+for i in range(len(apt_df)):
+    apt_lat = apt_df['coords'][i][0]['geometry']['location']['lat']
+    apt_lng = apt_df['coords'][i][0]['geometry']['location']['lng']
+    for j in range(len(college_df)):
+        college_lat = college_df['coords'][j][0]['geometry']['location']['lat']
+        college_lng = college_df['coords'][j][0]['geometry']['location']['lng']
+        distance = client.distance_matrix((apt_lat, apt_lng), (college_lat, college_lng), mode="walking")['rows'][0]['elements'][0]['distance']['text']
+        # Convert km to miles
+        distance = str(round((float(distance.split(" ")[0]) * 0.621371), 2)) + " miles"
 
-print(df['coords'])
+        # Add distance to df
+        new_row = pd.DataFrame({
+            'Apartment': [apt_df['Apartment'][i]],
+            'School': [college_df['College'][j]],
+            'Distance': [distance]
+        })
 
-# Get the latitude and longitude of two points
-lat1 = 37.4419
-lng1 = -122.1419
-lat2 = 37.7750
-lng2 = -122.4189
+        distance_df = pd.concat([distance_df, new_row], ignore_index=True)
 
-# Calculate the distance between the two points
-distance = client.distance_matrix((lat1, lng1), (lat2, lng2))['rows'][0]['elements'][0]['distance']['value']
-
-print(distance)
-
-
-geocode_result = []
-
-for i in range(2):
-    geocode_result.append.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-print(geocode_result)
+distance_df.to_csv('distance_data/distance_data.csv', index=False)
