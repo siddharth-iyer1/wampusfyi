64a5a449104902062aa0979d11be76d1de4c56ff
sahil-shah0203 <sahil0203@utexas.edu>
2023-10-21 15:16:21 -0500

    geocodeapi works!

--- distance_data/GoogleMapsAPI.py
+++ distance_data/GoogleMapsAPI.py
@@ -2,12 +2,23 @@ from googlemaps import Client
 import csv
 import pandas as pd
 
+"""
 apartments = csv.DictReader(open('/Users/siddharthiyer/Documents/GitHub/wampusfyi/distance_data/apartment_addresses.csv'))
 colleges = csv.DictReader(open('/Users/siddharthiyer/Documents/GitHub/wampusfyi/distance_data/college_addresses.csv'))
-
+"""
 api_key = "AIzaSyDoPCPo-aK28JSPXhMRHBdzL8jCpjrpvfc"
-
 client = Client(api_key)
+
+df = pd.read_csv("C:/Users/sahil/Desktop/CS Files/wampusfyi/distance_data/apartment_addresses.csv")
+
+geocode_result = []
+
+for i in df["Address"]:
+        geocode_result.append(client.geocode(i))
+
+df['coords'] = geocode_result
+
+print(df['coords'])
 
 # Get the latitude and longitude of two points
 lat1 = 37.4419
