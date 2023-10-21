fe47f7600e0f9418dc2f51c1921ca8ca59e8c44b
Siddharth <siddharth.iyer123@gmail.com>
2023-10-21 15:06:06 -0500

    put in csvs

---
+++ distance_data/GoogleMapsAPI.py
@@ -0,0 +1,29 @@
+from googlemaps import Client
+import csv
+import pandas as pd
+
+apartments = csv.DictReader(open('/Users/siddharthiyer/Documents/GitHub/wampusfyi/distance_data/apartment_addresses.csv'))
+colleges = csv.DictReader(open('/Users/siddharthiyer/Documents/GitHub/wampusfyi/distance_data/college_addresses.csv'))
+
+api_key = "AIzaSyDoPCPo-aK28JSPXhMRHBdzL8jCpjrpvfc"
+
+client = Client(api_key)
+
+# Get the latitude and longitude of two points
+lat1 = 37.4419
+lng1 = -122.1419
+lat2 = 37.7750
+lng2 = -122.4189
+
+# Calculate the distance between the two points
+distance = client.distance_matrix((lat1, lng1), (lat2, lng2))['rows'][0]['elements'][0]['distance']['value']
+
+print(distance)
+
+
+geocode_result = []
+
+for i in range(2):
+    geocode_result.append.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+print(geocode_result)
